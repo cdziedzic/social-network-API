c9dbe639b3219dd9a22543318297f24681ed7add
chris dziedzic <cdziedzic@gmail.com>
2023-09-17 17:32:28 -0400

    database successfully seeds

--- utils/data.js
+++ utils/data.js
@@ -1,121 +1,56 @@
-const names = [
-  'Aaran',
-  'Aaren',
-  'Aarez',
-  'Aarman',
-  'Aaron',
-  'Aaron-James',
-  'Aarron',
-  'Aaryan',
-  'Aaryn',
-  'Aayan',
-  'Aazaan',
-  'Abaan',
-  'Abbas',
-  'Abdallah',
-  'Abdalroof',
-  'Abdihakim',
-  'Abdirahman',
-  'Abdisalam',
-  'Abdul',
-  'Abdul-Aziz',
-  'Abdulbasir',
-  'Abdulkadir',
-  'Abdulkarem',
-  'Ze',
-  'Zechariah',
-  'Zeek',
-  'Zeeshan',
-  'Zeid',
-  'Zein',
-  'Zen',
-  'Zendel',
-  'Zenith',
-  'Zennon',
-  'Zeph',
-  'Zerah',
-  'Zhen',
-  'Zhi',
-  'Zhong',
-  'Zhuo',
-  'Zi',
-  'Zidane',
-  'Zijie',
-  'Zinedine',
-  'Zion',
-  'Zishan',
-  'Ziya',
-  'Ziyaan',
-  'Zohaib',
-  'Zohair',
-  'Zoubaeir',
-  'Zubair',
-  'Zubayr',
-  'Zuriel',
-  ``,
+const users = [
+{
+  username: 'user1',
+  email: "user1@test.com"
+},
+  {
+    username: 'user2',
+  email: 'user2@test.com'
+  },
+  {
+    username: 'user3',
+    email: 'user3@example.com'
+  },
+  {
+    username: 'user4',
+    email: 'user4@example.com'
+  }
 ];
 
-const descriptionsBodies = [
-  'How to disagree with someone',
-  'iPhone review',
-  'how-to video',
-  'video essay on the history of video games',
-  'How to make money on the App Store',
-  'Learn NextJS in five minutes (Not clickbate)',
-  'Movie trailer',
-  'Hello world',
-  'Another possible solution to the algorithm',
-  'Apology video',
-  'Submission for startup pitch',
+const thoughts = [
+  {
+    thoughtText: 'Random inspirational quote',
+    username: 'user1'
+  },
+  {
+    thoughtText: 'The earth is flat',
+    username: 'user2'
+  },
+  {
+    thoughtText: 'There is 5g in my vaccine!',
+    username: 'user2'
+  },
+  {
+    thoughtText: 'Just got my first coding job!',
+    username: 'user3'
+  },
+  {
+    thoughtText: 'I need to lose like 400 lbs',
+    username: 'user1'
+  }
+
 ];
 
-const possibleResponses = [
-  'I disagree!',
-  'I tried your algorithm, here were the results',
-  'This was awesome',
-  'Thank you for the great content',
-  'Please check out my video response',
-  'Like and subscribe to my channel please',
-  'Reply: The side effects of in app purchases on digital marketplaces',
+const reactions = [
+  {
+    reactionbody: 'I disagree!',
+    username: 'user4'
+  },
+  {
+   reactionbody: 'This was awesome',
+   username: 'user4'
+  }
 ];
 
-const users = [];
-
-// Get a random item given an array
-const getRandomArrItem = (arr) => arr[Math.floor(Math.random() * arr.length)];
-
-// Gets a random full name
-const getRandomName = () =>
-  `${getRandomArrItem(names)} ${getRandomArrItem(names)}`;
-
-// Function to generate random videos that we can add to the database. Includes video responses.
-const getRandomVideos = (int) => {
-  let results = [];
-  for (let i = 0; i < int; i++) {
-    results.push({
-      published: Math.random() < 0.5,
-      description: getRandomArrItem(descriptionsBodies),
-      advertiserFriendly: Math.random() < 0.5,
-      responses: [...getVideoResponses(3)],
-    });
-  }
-  return results;
-};
-
-// Create the responses that will be added to each video
-const getVideoResponses = (int) => {
-  if (int === 1) {
-    return getRandomArrItem(possibleResponses);
-  }
-  let results = [];
-  for (let i = 0; i < int; i++) {
-    results.push({
-      responseBody: getRandomArrItem(possibleResponses),
-      username: getRandomName(),
-    });
-  }
-  return results;
-};
-
-// Export the functions for use in seed.js
-module.exports = { getRandomName, getRandomVideos, getRandomVideos };
+// Export the data for use in seed.js
+module.exports = users, thoughts, reactions
--- utils/seed.js
+++ utils/seed.js
@@ -1,14 +1,17 @@
 const connection = require('../config/connection');
-const { User, Video } = require('../models');
-const { getRandomName, getRandomVideos } = require('./data');
+const { User, Video, Thought } = require('../models');
+const users = require('./data');
+const thoughts = require('./data');
+const reactions = require('./data');
+  
 
 connection.on('error', (err) => err);
 
 connection.once('open', async () => {
   console.log('connected');
   // Delete the collections if they exist
-  let videoCheck = await connection.db.listCollections({ name: 'videos' }).toArray();
-  if (videoCheck.length) {
+  let thoughtCheck = await connection.db.listCollections({ name: 'thoughts' }).toArray();
+  if (thoughtCheck.length) {
     await connection.dropCollection('videos');
   }
 
@@ -17,27 +20,13 @@ connection.once('open', async () => {
     await connection.dropCollection('users');
   }
 
-  const users = [];
-  const videos = getRandomVideos(10);
-
-  for (let i = 0; i < 20; i++) {
-    const fullName = getRandomName();
-    const first = fullName.split(' ')[0];
-    const last = fullName.split(' ')[1];
-
-    users.push({
-      first,
-      last,
-      age: Math.floor(Math.random() * (99 - 18 + 1) + 18),
-    });
-  }
 
   await User.collection.insertMany(users);
-  await Video.collection.insertMany(videos);
+  await Thought.collection.insertMany(thoughts);
 
   // loop through the saved videos, for each video we need to generate a video response and insert the video responses
   console.table(users);
-  console.table(videos);
+  console.table(thoughts);
   console.info('Seeding complete! 🌱');
   process.exit(0);
 });
